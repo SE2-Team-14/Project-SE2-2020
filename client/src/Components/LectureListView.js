7d2829c5afb7241205d4857161ddcd9c2ee0e791
Giacomo Garaccione <r.michelini@almaviva.it>
2020-11-16 00:38:03 +0100

    corrected 15mins of code smells
    commented a i++

--- client/src/Components/LectureListView.js
+++ client/src/Components/LectureListView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup, Col, Row, Jumbotron, Button, Modal} from 'react-bootstrap';
+import { ListGroup, Col, Row, Jumbotron, Button, Modal } from 'react-bootstrap';
 import API from '../api/API';
 
 
@@ -7,32 +7,32 @@ class LectureListView extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {lectures: [], showBookSuccess: false, names: []};
+        this.state = { lectures: [], showBookSuccess: false, names: [] };
     }
-    componentDidMount(){
+    componentDidMount() {
         API.getLecturesList(this.props.email)
-        .then((lectures) => this.setState({lectures : lectures}, ()=> this.state.lectures.map((l) => API.getCourseName(l.courseId).then((name) => this.state.names.push(name)))));
+            .then((lectures) => this.setState({ lectures: lectures }, () => this.state.lectures.map((l) => API.getCourseName(l.courseId).then((name) => this.state.names.push(name)))));
     }
 
-   /* getCourseName = (courseId) => {
-        API.getCourseName(courseId).then((name)=> console.log(name));
-    }*/
-/*
-    getTeacherName = (courseId) => {
-        API.getTeacherName(courseId).then((name)=> this.setState({teacherName: name}));
-    }
-
-    getMaxSeats = (classroom) => {
-        API.getMaxSeats(classroom).then((maxSeats) => this.setState({maxSeats: maxSeats}));
-    }*/
+    /* getCourseName = (courseId) => {
+         API.getCourseName(courseId).then((name)=> console.log(name));
+     }*/
+    /*
+        getTeacherName = (courseId) => {
+            API.getTeacherName(courseId).then((name)=> this.setState({teacherName: name}));
+        }
+    
+        getMaxSeats = (classroom) => {
+            API.getMaxSeats(classroom).then((maxSeats) => this.setState({maxSeats: maxSeats}));
+        }*/
     //handleBookClick = (lecture, lectureId) => {
     //    this.setState({showBookSuccess: true, booklecture: lecture, booklectureId: lectureId});
     //}
 
-   // handleBook = (event, lecture, lectureId) => {
-   //     event.preventDefault();
-        
-   // }
+    // handleBook = (event, lecture, lectureId) => {
+    //     event.preventDefault();
+
+    // }
 
     //Book = (lectureId) =>{
     //    API.bookLecture
@@ -47,7 +47,7 @@ class LectureListView extends React.Component {
             <Jumbotron className='d-flex justify-content-around col-12 m-0 p-3'>
                 <Row className='col-12 m-0 p-0'>
                     <Col>
-                        <LectureList lecture={this.state.lectures} names = {this.state.names} getCourseName={this.getCourseName} getTeacherName={this.getTeacherName} getMaxSeats={this.getMaxSeats}/>
+                        <LectureList lecture={this.state.lectures} names={this.state.names} getCourseName={this.getCourseName} getTeacherName={this.getTeacherName} getMaxSeats={this.getMaxSeats} />
                     </Col>
                 </Row>
                 <Modal controlid='BookSuccess' show={this.state.showBookSuccess} onHide={this.handleClose} animation={false} >
@@ -100,12 +100,12 @@ function LectureList(props) {
 
             </ListGroup.Item>
             {
-               props.lecture.map((l) => 
-                   
-                     <LectureItem key={l.lectureId}  names = {props.names} lecture = {l} getCourseName = {props.getCourseName} classroom={l.classroomId} getTeacherName={props.getTeacherName} getMaxSeats={props.getMaxSeats}/>
-                     )                  
-                  
-                  
+                props.lecture.map((l) =>
+
+                    <LectureItem key={l.lectureId} names={props.names} lecture={l} getCourseName={props.getCourseName} classroom={l.classroomId} getTeacherName={props.getTeacherName} getMaxSeats={props.getMaxSeats} />
+                )
+
+
             }
         </ListGroup>
 
@@ -118,10 +118,10 @@ function LectureItem(props) {
     //props.getCourseName(props.courseId);
     /*let teacherName = props.getTeacherName(props.courseId);
     let maxSeats = props.getMaxSeats(props.classroom);*/
-   // let courseName = props.getCourseName(props.lecture.courseId);
-   console.log("Prova");
-   let name = props.names.map((n => console.log(n)));
-   console.log(name);
+    // let courseName = props.getCourseName(props.lecture.courseId);
+    console.log("Prova");
+    let name = props.names.map((n => console.log(n)));
+    console.log(name);
     return (
         <ListGroup.Item className='border mt-1'>
             <Row className='justify-content-around'>
@@ -150,12 +150,12 @@ function LectureItem(props) {
                     <Button>Book</Button>
                 </Col>
                 <Col xs={1} className='text-center'>
-                    
+
                 </Col>
             </Row>
         </ListGroup.Item>
     );
-    i++;
+    //i++;
 }
 
 
