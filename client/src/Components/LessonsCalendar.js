984525ebd9e9e01033dd27cd204fd2f50feb3b0f
Gaetano <s277875@studenti.polito.it>
2020-11-24 21:55:37 +0100

    Calendar

---
+++ client/src/Components/LessonsCalendar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//run npm install @syncfusion/ej2-react-schedule
+//npm install @syncfusion/ej2-react-grids --save
+//npm install @syncfusion/ej2-react-buttons --save
+
+class LessonsCalendar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        //const { data } = this.state;
+
+        return (
+          <Button>ciao</Button>
+        );
+    }
+}
+
+export default LessonsCalendar;
