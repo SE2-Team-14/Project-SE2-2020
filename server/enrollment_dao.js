76c35219b1c3c628b1fefe62b40ba26e5736a12f
Gaetano <s277875@studenti.polito.it>
2020-11-25 22:38:01 +0100

    Cleared production db

--- server/enrollment_dao.js
+++ server/enrollment_dao.js
@@ -52,3 +52,40 @@ exports.getEnrolledStudentsByCourseName = function (courseName) {
         });
     });
 }
+
+/*
+exports.getEnrolledStudents = function (courseId) {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM ENROLLMENT WHERE courseId = ?";
+        db.all(sql, [courseId], (err, row) => {
+            if (err)
+                reject(err);
+            else {
+                if (row > 0) {
+                    let _students = rows.map((stud => Person.createPerson(stud)))[0];
+                    resolve(_students);
+                }
+                else
+                    resolve(undefined);
+            }
+        });
+    });
+}
+
+exports.getCourses = function (studentId) {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM ENROLLMENT WHERE email = ?";
+        db.all(sql, [email], (err, row) => {
+            if (err)
+                reject(err);
+            else {
+                if (row > 0) {
+                    let _courses = rows.map((course => Course.createCourse(course)))[0];
+                    resolve(_courses);
+                }
+                else
+                    resolve(undefined);
+            }
+        });
+    });
+}*/
