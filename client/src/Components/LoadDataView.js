9af5c21f51f38242dab79a3128017c8f9f16498c
GaetanoEpiro <s277875@studenti.polito.it>
2020-12-15 13:52:31 +0100

    Improve frontend quality

--- client/src/Components/LoadDataView.js
+++ client/src/Components/LoadDataView.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Jumbotron, Button } from 'react-bootstrap';
 
 const Papa = require('papaparse');
 
-const LoadDataView = () => {
-  const [file, setFile] = useState("");
-  const [inputType, setFileType] = useState("");
-  const [filename, setFilename] = useState('Choose File');
+const studentHeader = "Id,Name,Surname,City,OfficialEmail,Birthday,SSN";
+const teacherHeader = "Number,GivenName,Surname,OfficialEmail,SSN";
+const enrollmentHeader = "Code,Student";
+const scheduleHeader = "Code,Room,Day,Seats,Time";
+const coursesHeader = "Code,Year,Semester,Course,Teacher";
 
-  const studentHeader = "Id,Name,Surname,City,OfficialEmail,Birthday,SSN";
-  const teacherHeader = "Number,GivenName,Surname,OfficialEmail,SSN";
-  const enrollmentHeader = "Code,Student";
-  const scheduleHeader = "Code,Room,Day,Seats,Time";
-  const coursesHeader = "Code,Year,Semester,Course,Teacher";
+class LoadDataView extends React.Component {
 
-  const handleFile = (e) => {
+  state = {
+    // Initially, no file is selected 
+    selectedFile: null,
+    fileType: ""
+  };
+
+  handleFile = (e) => {
     const content = e.target.result.toString();
     let header = "";
 
@@ -28,19 +31,19 @@ const LoadDataView = () => {
 
     switch (header.join(",")) {
       case studentHeader:
-        setFileType("student");
+        this.setState({ fileType: "student" });
         break;
       case teacherHeader:
-        setFileType("teacher");
+        this.setState({ fileType: "teacher" });
         break;
       case enrollmentHeader:
-        setFileType("enrollment");
+        this.setState({ fileType: "enrollment" });
         break;
       case scheduleHeader:
-        setFileType("schedule");
+        this.setState({ fileType: "schedule" });
         break;
       case coursesHeader:
-        setFileType("course");
+        this.setState({ fileType: "course" });
         break;
       default:
         alert("The file does not contains students or the format is wrong!");
@@ -48,46 +51,65 @@ const LoadDataView = () => {
     }
   }
 
-  const onChange = (e) => {
+  onChange = e => {
     let file = e.target.files[0]
     let fileData = new FileReader();
-    fileData.onloadend = handleFile;
+    fileData.onloadend = this.handleFile;
     fileData.readAsText(file);
-
-    setFile(e.target.files[0]);
-    setFilename(e.target.files[0].name);
+    this.setState({ selectedFile: file });
   }
 
-  const onSubmit = async e => {
+  onSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('file', file);
+    if (this.state.selectedFile != null) {
+      const formData = new FormData();
+      formData.append('file', this.state.selectedFile);
 
-    try {
-      const res = await axios.post(`/upload/${inputType}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      try {
+        axios.post(`/upload/${this.state.fileType}`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
 
-      console.log('File Uploaded');
-    } catch (err) {
-      if (err.response.status === 500) {
-        console.log('There was a problem with the server');
-      } else {
-        console.log(err.response.data.msg);
+        console.log('File Uploaded');
+      } catch (err) {
+        if (err.response.status === 500) {
+          console.log('There was a problem with the server');
+        } else {
+          console.log(err.response.data.msg);
+        }
       }
     }
   };
 
-  return (
-    <Jumbotron className="text-center">
-      <h4>Select a file to load data into the system </h4>
-      <p></p>
-      <input type="file" id="students" accept=".csv" onChange={onChange} />
-      <Button onClick={onSubmit}>Submit</Button>
-    </Jumbotron>
-  );
+  fileData = () => {
+
+    if (this.state.selectedFile) {
+      return (
+        <div>
+          <p> </p>
+          <h4>File Details:</h4>
+          <p>File Name: {this.state.selectedFile.name}</p>
+        </div>
+      );
+    }
+  };
+
+  render() {
+
+    return (
+      <Jumbotron className="text-center">
+        <h4>Select a file to load data into the system </h4>
+        <p></p>
+        <div>
+          <input type="file" accept=".csv" onChange={this.onChange} />
+          <Button onClick={this.onSubmit}>Upload!</Button>
+        </div>
+        {this.fileData()}
+      </Jumbotron>
+    );
+  }
 };
 
 export default LoadDataView;
