b72df9588aa97351466175a0f4ab3a25ee909b91
ALESSANDRO LEPORI s280132 <s280132@studenti.polito.it>
2020-12-29 19:19:59 +0100

    add semester view

--- client/src/Components/UpdateBookableLecture.js
+++ client/src/Components/UpdateBookableLecture.js
@@ -34,16 +34,7 @@ class UpdateBookableLecture extends React.Component {
                                 <Col xs={5}> <h3>Do you want update for year or for semester?</h3> </Col>
                                 <Col xs={1}> <Button class="btn btn-secondary btn-sm" onClick={() => this.modeYear()}> Year </Button> </Col>
                                 <Col xs={1}> <Button class="btn btn-secondary btn-sm" onClick={() => this.modeSemester()}> Semester </Button> </Col>
-                                <Row className = 'col-6 m-0 p-0'>
-                                 {this.state.showYear &&
-                                    <YearList></YearList>
-                                 }
-                                {this.state.showSemester &&
-                                    <SemesterList></SemesterList>
-                                }
-                                </Row>
                             </Row>                
-
                             <Modal>
                                 <Modal.Header>
                                     <Modal.Title></Modal.Title>
@@ -55,6 +46,17 @@ class UpdateBookableLecture extends React.Component {
                                 </Modal.Footer>
                             </Modal>
                         </Jumbotron>
+                        {this.state.showYear &&
+                        <Jumbotron className='d-flex justify-content-around col-12 m-0 p-3'>
+                                 <YearList></YearList>
+                         </Jumbotron>
+                        }
+                        {this.state.showSemester &&
+                        <Jumbotron className='d-flex justify-content-around col-12 m-0 p-3'>
+                            <SemesterList></SemesterList>
+                        </Jumbotron>
+                }
+                        
                     </>
                 )}
             </AuthContext.Consumer>
@@ -63,14 +65,15 @@ class UpdateBookableLecture extends React.Component {
 
 }
 function YearList() {
-    const years = ['1', '2', '3', '4', '5'];
+    const years = [1, 2, 3, 4, 5];
     return (
         <Container>
-            <Row>
+            <Row><h4>Select in the following list, the year or the year that you want to update!</h4></Row>
+            <Row className = 'text-center'>
                 <Col xs={0.1} className='text-center'>
                     <Form.Check type={"checkbox"} id={years[0]} />
                 </Col>
-                <Col xs={1} className='text-center'>
+                <Col xs={1.5} className='text-center'>
                     Year 1
                 </Col>
             </Row>
@@ -78,7 +81,7 @@ function YearList() {
                 <Col xs={0.1} className='text-center'>
                     <Form.Check type={"checkbox"} id={years[1]} />
                 </Col>
-                <Col xs={1} className='text-center'>
+                <Col xs={1.5} className='text-center'>
                     Year 2
                 </Col>
             </Row>
@@ -86,7 +89,7 @@ function YearList() {
                 <Col xs={0.1} className='text-center'>
                     <Form.Check type={"checkbox"} id={years[2]} />
                 </Col>
-                <Col xs={1} className='text-center'>
+                <Col xs={1.5} className='text-center'>
                     Year 3
                 </Col>
             </Row>
@@ -94,7 +97,7 @@ function YearList() {
                 <Col xs={0.1} className='text-center'>
                     <Form.Check type={"checkbox"} id={years[3]} />
                 </Col>
-                <Col xs={1} className='text-center'>
+                <Col xs={1.5} className='text-center'>
                     Year 4
                 </Col>
             </Row>
@@ -102,16 +105,53 @@ function YearList() {
                 <Col xs={0.1} className='text-center'>
                     <Form.Check type={"checkbox"} id={years[4]} />
                 </Col>
-                <Col xs={1} className='text-center'>
+                <Col xs={1.5} className='text-center'>
                     Year 5
                 </Col>
             </Row>
+            <Row>
+                <h4>     </h4>
+            <Button> Confirm selection </Button>
+            </Row>
+            
         </Container>
     );
 }
 
 function SemesterList() {
 
+    const semesters = [1, 2];
+    return (
+        <Container>
+            <Row><h4>Select first the year and after the semester/semesters that you want to update</h4></Row>
+            <Row>
+                <Button class="btn btn-secondary btn-sm"> Year 1 </Button>
+                <Button class="btn btn-secondary btn-sm"> Year 2 </Button>
+                <Button class="btn btn-secondary btn-sm"> Year 3 </Button>
+                <Button class="btn btn-secondary btn-sm"> Year 4 </Button>
+                <Button class="btn btn-secondary btn-sm"> Year 5 </Button>
+            </Row>
+            <Row>
+                <Col xs={0.1} className='text-center'>
+                    <Form.Check type={"checkbox"} id={semesters[0]} />
+                </Col>
+                <Col xs={1.5} className='text-center'>
+                    Semester 1
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={0.1} className='text-center'>
+                    <Form.Check type={"checkbox"} id={semesters[1]} />
+                </Col>
+                <Col xs={1.5} className='text-center'>
+                    Semester 2
+                </Col>
+            </Row>
+            <Row>
+                <Button>Confirm selection</Button>
+            </Row>
+        </Container>
+    );
 }
 
 export default UpdateBookableLecture;
